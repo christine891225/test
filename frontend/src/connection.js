a0a59fdf88b9c5a674844882a78ea950446772e0
christine891225 <64571229+christine891225@users.noreply.github.com>
2022-12-24 09:21:59 +0800

    for deployment

--- frontend/src/connection.js
+++ frontend/src/connection.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
-const API_PORT = 
-  process.env.NODE_ENV === "production"
-  ? "/api"
-  : "http://localhost:4000/api";
+// const API_PORT = 
+//   process.env.NODE_ENV === "production"
+//   ? "/api"
+//   : "http://localhost:4000/api";
+
+const API_PORT = "/api";
 
 export const api = axios.create({ baseURL: API_PORT });
