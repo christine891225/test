d56821284d63ae4e50b84a86418ebd0560147228
christine891225 <64571229+christine891225@users.noreply.github.com>
2022-12-24 09:00:35 +0800

    for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,6 +5,8 @@ dotenv.config();
 import mongoose from 'mongoose';
 import routes from './routes';
 
+import path from "path";
+
 mongoose.connect(
   process.env.MONGO_URL,{
     useNewUrlParser: true,
@@ -12,9 +14,19 @@ mongoose.connect(
 }).then((res) => console.log("mongo db connection created"));
 
 const app = express();
-app.use(express.json());
-app.use(cors());
-app.use('/', routes);
+if (process.env.NODE_ENV === "development") {
+  app.use(express.json());
+  app.use(cors());
+  app.use('/', routes);
+}
+
+if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, "../frontend", "build")));
+  app.get("/*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 4000;
 app.get('/', (req, res) => {
